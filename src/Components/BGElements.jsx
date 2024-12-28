c604530e583445f84607339a5f3834854e5fe470
anantkatyayn <anantkatyayn112@gmail.com>
2024-12-28 15:07:47 +0530

    beautified moving elements

--- src/Components/BGElements.jsx
+++ src/Components/BGElements.jsx
@@ -39,6 +39,41 @@ const BGElements = () => {
         <div className="w-5 h-5 border-2 border-white rounded-full"></div>{" "}
         {/* Circle */}
       </div>
+    <div className="absolute left-1 top-5 animate-random-motion z-10">
+        <div className="w-8 h-8 border-2 border-white rounded-full"></div>{" "}
+        {/* Circle */}
+      </div>
+      <div className="absolute right-20 top-16 animate-random-motion z-10">
+        <div className="w-6 h-6 border-2 border-white transform rotate-45"></div>{" "}
+        {/* Diamond */}
+      </div>
+      <div className="absolute left-[20%] bottom-[10%] animate-random-motion z-10">
+        <div className="w-8 h-8 border-2 border-white transform rotate-45"></div>{" "}
+        {/* Triangle */}
+      </div>
+      <div className="absolute right-10 bottom-20 animate-random-motion z-10">
+        <div className="w-6 h-6 border-2 border-white flex items-center justify-center">
+          <span className="text-white text-sm font-bold transform rotate-45">
+            +
+          </span>{" "}
+          {/* Plus */}
+        </div>
+      </div>
+      <div className="absolute left-[10%] top-[20%] animate-random-motion z-10">
+        <div className="w-5 h-5 border-2 border-white rounded-full"></div>{" "}
+        {/* Small Circle */}
+      </div>
+      <div className="absolute right-[3%] top-[90%] animate-random-motion z-10">
+        <div className="w-7 h-7 border-2 border-white"></div> {/* Square */}
+      </div>
+      <div className="absolute left-[10%] top-[5%] animate-random-motion z-10">
+        <div className="w-6 h-6 border-2 border-white transform rotate-45"></div>{" "}
+        {/* Diamond */}
+      </div>
+      <div className="absolute right-[16%] bottom-[26%] animate-random-motion z-10">
+        <div className="w-5 h-5 border-2 border-white rounded-full"></div>{" "}
+        {/* Circle */}
+      </div>
     </>
   )
 }
